cd462061edb20e53b80cefc009504e1a958d7def
Ivan S <ivnoranot@gmail.com>
2024-11-05 21:19:08 +0500

    it works fine

--- crawl.js
+++ crawl.js
@@ -1,3 +1,5 @@
+const {JSDOM} = require('jsdom')
+
 function normalizeURL(url) {
     const url_object = new URL(url)
     const hostname_path = `${url_object.hostname}${url_object.pathname}`
@@ -7,6 +9,32 @@ function normalizeURL(url) {
     return hostname_path
 }
 
+function getURLfromHTMLcode(htmlCode, baseURL) {
+    const urls = []
+    const dom = new JSDOM(htmlCode)
+    const links = dom.window.document.querySelectorAll('a')
+    for (const link of links) {
+        if (link.href.slice(0, 1) == '/') { // relative url
+            try {
+                const url_object = new URL(`${baseURL}${link.href}`) // create errors if sth wrong
+                urls.push(url_object.href)
+            } catch (error) { // error is an object
+                console.log(`error. something wrong with the url: ${error.message}`)
+            }
+        } else { // absolute url
+            try {
+                const url_object = new URL(link.href) // create errors if something wrong
+                urls.push(url_object.href)
+            } catch (error) { // an object
+                console.log(`error. something wrong with the url: ${error.message}`)
+            }
+        }
+
+    }
+    return urls
+}
+
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    getURLfromHTMLcode,
 }
--- crawl.test.js
+++ crawl.test.js
@@ -1,4 +1,4 @@
-const {normalizeURL} = require('./crawl.js')
+const {normalizeURL, getURLfromHTMLcode} = require('./crawl.js')
 const {test, expect} = require('@jest/globals')
 
 test('normalizeURL return hostname', () => {
@@ -23,8 +23,70 @@ test('normalizeURL remove capitals', () => {
 })
 
 test('normalizeURL remove http', () => {
-    const input = 'https://google.google.com/'
+    const input = 'http://google.google.com/'
     const output = normalizeURL(input)
     const expected_output = 'google.google.com'
+    expect(output).toEqual(expected_output)
+})
+
+test('getURLfromHTMLcode', () => {
+    const HTMLinput = `
+        <html><body>
+        <a href='https://google.com/'>click</a>
+        </body></html>
+    `
+    const URLinput = "https://google.com" 
+    const output = getURLfromHTMLcode(HTMLinput, URLinput)
+    const expected_output = ['https://google.com/']
+    expect(output).toEqual(expected_output)
+})
+
+test('getURLfromHTMLcode get relative url', () => {
+    const HTMLinput = `
+        <html><body>
+        <a href='/path/'>click</a>
+        </body></html>
+    `
+    const URLinput = "https://google.com" 
+    const output = getURLfromHTMLcode(HTMLinput, URLinput)
+    const expected_output = ['https://google.com/path/']
+    expect(output).toEqual(expected_output)
+})
+
+test('getURLfromHTMLcode get absolue url', () => {
+    const HTMLinput = `
+        <html><body>
+        <a href='https://google.com/path/'>click</a>
+        </body></html>
+    `
+    const URLinput = "https://google.com" 
+    const output = getURLfromHTMLcode(HTMLinput, URLinput)
+    const expected_output = ['https://google.com/path/']
+    expect(output).toEqual(expected_output)
+})
+
+test('getURLfromHTMLcode get both urls', () => {
+    const HTMLinput = `
+        <html><body>
+        <a href='https://google.com'>click</a>
+        <a href='/path/'>click</a>
+        </body></html>
+    `
+    const URLinput = "https://google.com" 
+    const output = getURLfromHTMLcode(HTMLinput, URLinput)
+    const expected_output = ['https://google.com/', 'https://google.com/path/']
+    expect(output).toEqual(expected_output)
+})
+
+test('getURLfromHTMLcode wrong urls', () => {
+    const HTMLinput = `
+        <html><body>
+        <a href='unacceptable_url/'>click</a> 
+        <a href='unacceptable url'>click</a> 
+        </body></html>
+    `
+    const URLinput = "https://google.com" 
+    const output = getURLfromHTMLcode(HTMLinput, URLinput)
+    const expected_output = []
     expect(output).toEqual(expected_output)
 })
